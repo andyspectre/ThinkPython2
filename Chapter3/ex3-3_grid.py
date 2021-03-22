0a59d173acd94701e08d08e12cf4f51f9d385e6b
Andrea Cocco <coccoa@pm.me>
2021-03-22 21:03:23 +0100

    Update ex3-3_grid.py

--- Chapter3/ex3-3_grid.py
+++ Chapter3/ex3-3_grid.py
@@ -1,39 +1,26 @@
-"""Andrea Cocco 2020
-Chapter 3 Exercise 3 from the book:
+# Andrea Cocco 2020
+# Chapter 3 Exercise 3 of the book:
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpython2.com
+#
+# Note: This exercise should be done using only the statements
+# and other features we have learned so far.
 
-Think Python, 2nd Edition
-by Allen Downey
-http://thinkpython2.com
+# Write a function that draws a grid like the following:
+# + - - - - + - - - - +
+# |         |         |
+# |         |         |
+# |         |         |
+# |         |         |
+# + - - - - + - - - - +
+# |         |         |
+# |         |         |
+# |         |         |
+# |         |         |
+# + - - - - + - - - - +
 
-Note: This exercise should be done using only the statements and other features we have learned so far.
-
-    1. Write a function that draws a grid like the following:
-
-    + - - - - + - - - - +
-    |         |         |
-    |         |         |
-    |         |         |
-    |         |         |
-    + - - - - + - - - - +
-    |         |         |
-    |         |         |
-    |         |         |
-    |         |         |
-    + - - - - + - - - - +
-
-    Hint: to print more than one value on a line, you can print a comma-separated sequence of values:
-
-    print('+', '-')
-
-    By default, print advances to the next line, but you can override that behavior and put a space at the end, like this:
-
-    print('+', end=' ')
-    print('-')
-
-    The output of these statements is '+ -' on the same line. The output from the next print statement would begin on the next line.
-    """
-    
-    def do_twice(f):
+def do_twice(f):
     f()
     f()
 
