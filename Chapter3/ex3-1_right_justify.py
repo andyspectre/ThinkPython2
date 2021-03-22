e66e150684dfb189c74f178582a84e17fae80752
Andrea Cocco <coccoa@pm.me>
2021-03-22 20:56:50 +0100

    Update ex3-1_right_justify.py

--- Chapter3/ex3-1_right_justify.py
+++ Chapter3/ex3-1_right_justify.py
@@ -1,19 +1,13 @@
-"""Andrea Cocco 2020
-Chapter 3 Exercise 1 from the book:
-
-Think Python, 2nd Edition
-by Allen Downey
-http://thinkpython2.com
-
-Write a function named right_justify that takes a string named s as a parameter and prints the string 
-with enough leading spaces so that the last letter of the string is in column 70 of the display:
-
->>> right_justify('monty')
-                                                                 monty
-
-Hint: Use string concatenation and repetition. Also, Python provides a built-in function called len 
-that returns the length of a string, so the value of len('monty') is 5.
-"""
+# Andrea Cocco 2020
+# Chapter 3 Exercise 1 from the book:
+# 
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpython2.com
+# 
+# Write a function named right_justify that takes a string names s as a 
+# parameter and prints the string with enough leading spaces so that the
+# last letter of the string is in column 70 of the display:
 
 def right_justify(s):
     spaces = 70-len(s)
