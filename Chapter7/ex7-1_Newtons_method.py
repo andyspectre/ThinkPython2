6e5bb309f812875e8cdaa6bd48e476a96d6a417d
Andrea Cocco <coccoa@pm.me>
2021-04-19 19:30:51 +0200

    Update ex7-1_Newtons_method.py

--- Chapter7/ex7-1_Newtons_method.py
+++ Chapter7/ex7-1_Newtons_method.py
@@ -1,3 +1,28 @@
+# Andrea Cocco 2020
+# Chapter 7 Exercise 1 from the book:
+#     
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpython2.com
+#
+# write a function named test_square_root
+# that prints a table like this:
+# a   mysqrt(a)     math.sqrt(a)  diff
+# -   ---------     ------------  ----
+# 1.0 1.0           1.0           0.0
+# 2.0 1.41421356237 1.41421356237 2.22044604925e-16
+# 3.0 1.73205080757 1.73205080757 0.0
+# 4.0 2.0           2.0           0.0
+# 5.0 2.2360679775  2.2360679775  0.0
+# 6.0 2.44948974278 2.44948974278 0.0
+# 7.0 2.64575131106 2.64575131106 0.0
+# 8.0 2.82842712475 2.82842712475 4.4408920985e-16
+# 9.0 3.0           3.0           0.0
+# The first column is a number, a, the second column is the square
+# root of a computed with mysqrt; the third column is the
+# square root computed by math.sqrt; the fourth column is the
+# absolute value of the difference between the two estimates.
+
 import math
 
 def mysqrt(v):
@@ -15,7 +40,7 @@ def mysqrt(v):
 def test_square_root(v):
     """Print a table that, for all the numbers in the range v, 
     compares the square roots calculated with the 
-    Newton's method with those calculated with the built in function sqrt()
+    Newton's method with those calculated with the built in function math.sqrt()
     and display the absolute error between the two.
     """
     n = float(1) 
