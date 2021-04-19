75079f14a09ed874d54f9b6fb2ca2fb89088ad72
Andrea Cocco <coccoa@pm.me>
2021-04-19 17:41:48 +0200

    Update ex6-3_palindrome.py

--- Chapter6/ex6-3_palindrome.py
+++ Chapter6/ex6-3_palindrome.py
@@ -1,3 +1,14 @@
+# Andrea Cocco 2020
+# Chapter 6 Exercise 2 from the book:
+#     
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpython2.com
+
+# Write a function called is_palindrome that takes
+# a string argument and returns True if it is a palindrome
+# and False otherwise. Remember that you can use the
+# built-in function len to check the length of a string.
 
 def is_palindrome(myword):
     """Check if a word is a palindrome"""
@@ -7,7 +18,7 @@ def is_palindrome(myword):
     # If first and last word are different, it is NOT a palindrome
     if  myword[0] != myword[-1]:
         return False
-    # call again this function, this time excluding first and last characters
+    # Call again this function, this time excluding first and last characters
     return is_palindrome(myword[1:-1])
 
 def ask_word():
