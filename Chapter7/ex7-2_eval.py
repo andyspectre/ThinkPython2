ad234d0ea76da86f7aaa07001ecdb74a6508d16a
Andrea Cocco <coccoa@pm.me>
2021-04-19 20:05:37 +0200

    Update ex7-2_eval.py

--- Chapter7/ex7-2_eval.py
+++ Chapter7/ex7-2_eval.py
@@ -1,3 +1,14 @@
+# Andrea Cocco 2020
+# Chapter 7 Exercise 2 from the book:
+#     
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpython2.com
+#
+Write a function called eval_loop that iteratively
+prompts the user, takes the resulting input and evaluates
+it using eval, and prints the result. It should continue until the user enters 'done'.
+
 def ask_user():
     val = input('Insert a value, enter "done" to exit: ')
     if val != 'done':
