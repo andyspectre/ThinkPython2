e269a8a4526dcf112fde403c6a3c607879721fa7
Andrea Cocco <coccoa@pm.me>
2021-04-30 19:01:25 +0200

    Add comments

--- chap08-strings/ex8-3-palindrome2.py
+++ chap08-strings/ex8-3-palindrome2.py
@@ -1,3 +1,14 @@
+# Andrea Cocco 2020
+# Chapter 8 Exercise 3 from the book:
+#     
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpython2.com
+#
+# A step size of -1 goes through the word backwards, so the slice [::-1]
+# generates a reversed string. Use this idiom to write a one-line 
+# version of is_palindrome from Exercise 6-3
+
 def is_palindrome(w):
     return w == w[::-1]
 
