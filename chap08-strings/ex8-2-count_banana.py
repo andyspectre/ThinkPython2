5122e044adcf2f7633afe826ffca592eaf8ced93
Andrea Cocco <coccoa@pm.me>
2021-04-30 18:32:20 +0200

    Added comments

--- chap08-strings/ex8-2-count_banana.py
+++ chap08-strings/ex8-2-count_banana.py
@@ -1,3 +1,15 @@
+# Andrea Cocco 2020
+# Chapter 8 Exercise 2 from the book:
+#     
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpython2.com
+#
+# There is a string method called count that is
+# similar to the function in "Looping and Counting" 
+# on page 89. Read the documentation of this method and write 
+# an invocation that counts the number of a's in 'banana'.
+
 fruit = 'banana'
 
 print(fruit.count('a'))
