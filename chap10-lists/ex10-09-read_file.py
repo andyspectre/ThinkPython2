ddce7364dc24bed7dfa25ce3630f619f20f84653
c0cc0a <coccoa@pm.me>
2021-05-18 17:49:00 +0200

    add exercise 10-09

---
+++ chap10-lists/ex10-09-read_file.py
@@ -0,0 +1,20 @@
+# Andrea Cocco 2020
+# Chapter 10 Exercise 7 from the book:
+#     
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpython2.com
+#
+# Write a function that reads a txt file and builds a 
+# list with one element per word. 
+
+def mylist(f):
+    myli = []
+    for line in f:
+        word = line.strip()
+        myli.append(word) 
+    return myli
+
+
+fin = open('pass_list.txt')
+print(mylist(fin))
