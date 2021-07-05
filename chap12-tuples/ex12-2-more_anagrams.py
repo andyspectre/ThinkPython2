84c70cc3efc10ee722ac23811292555f37825440
c0cc0a <coccoa@pm.me>
2021-07-05 17:30:12 +0200

    add pseudocode, add function create_list() and key_getter()

--- chap12-tuples/ex12-2-more_anagrams.py
+++ chap12-tuples/ex12-2-more_anagrams.py
@@ -1,19 +1,39 @@
-def more_anagrams(s):
+""" 1. Read file and initialize a list of strings with it
+    2. key_getter(word)
+        for each letter of the word:
+            get its equivalent unicode number 
+            put it in a tuple called key
+    3. more_anagrams(tuple, word):
+           initialize dictionary
+           if tuple is not in dictionary:
+               key is the tuple and value is word
+           else:
+               add word to that key
+        key = tuple with numeric representation of the letters of the strings
+        value = list of words that match that key
+"""
+import string
+
+def key_getter(strs):
+    key = [] 
+    for letter in strs:
+        if letter.islower():
+            start = ord('a')
+        elif letter.isupper():
+            start = ord('A')
+        position = ord(letter) - start
+        key.append(position)
+    return tuple(key)
+
+
+def create_list(strs):
     li = []
-    for line in s:
+    for line in strs:
         word = line.strip()
         li.append(word)
-    li.sort()
+    # return li
 
-    d = dict()
-    for word in li:
-        keyes = [len(word)]
-        for key in keyes:
-            if key not in d:
-                d[key] = [word]
-            else:
-                d[len(word)].append(word)
-    return d
 
-fin = open("wordliswordlist.txt")
-print(more_anagrams(fin))
+
+fin = open("little_anagram_list.txt")
+print(key_getter('tea'))
