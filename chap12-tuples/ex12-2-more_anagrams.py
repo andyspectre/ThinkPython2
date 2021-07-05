97b20f0a1c8dbd04cfebf3ed486108b1430ca98c
c0cc0a <coccoa@pm.me>
2021-07-05 19:41:07 +0200

    clean up and add comments

--- chap12-tuples/ex12-2-more_anagrams.py
+++ chap12-tuples/ex12-2-more_anagrams.py
@@ -15,30 +15,41 @@
 import string
 
 def result(d):
+    """ Return a list of words that are anagrams """
     r = []
     for key in d:
+        # we want only the elements in the dictionary that have at least
+        # two words, that is they are anagrams
         if len(d[key]) > 1:
             r.append(d[key])
     return r
 
 def key_getter(strs):
-    key = [0] * 26
+    """ Return a tuple of "markers" that mark which letters
+    appear in a string. These markers will be the keyes of a dictionary
+    """
+    key = [0] * 26    # initialize a list of 26 "0" (our markers)
     for letter in strs:
+        # get the unicode representation of the letters of the words
         if letter.islower():
             start = ord('a')
         elif letter.isupper():
             start = ord('A')
         position = ord(letter) - start
-        key[position] += 1
-    key = tuple(key)
+
+        key[position] += 1    # mark the letters (just add '1' in the list)
+
+    # convert list into tuple so that can be used as key in a dictionary
+    key = tuple(key)    
     return key
 
-def create_list(strs):
-    li = []
+def anagram(li):
+    """ Accept a list of strings as argument and return a dictionary 
+    in which the key is a tuple of "markers" representing the letters 
+    of the words and the value is the actual word.
+    """
     d = dict()
-    for line in strs:
-        word = line.strip()
-        li.append(word)
+
     for word in li:
         key = key_getter(word)
         if key not in d:
@@ -47,5 +58,16 @@ def create_list(strs):
             d[key].append(word)
     return result(d)
 
-fin = open("file.txt")
+def create_list(strs):
+    """ Accept a file with one string per line and return a list
+    of strings.
+    """
+    li = []
+    for line in strs:
+        word = line.strip()
+        li.append(word)
+    return anagram(li)
+
+
+fin = open("pass_list.txt")
 print(create_list(fin))
