1da32fc6ecc5925b72ceb762f9ba463b4da5923b
c0cc0a <coccoa@pm.me>
2021-09-01 17:34:25 +0200

    add exercise 13-1

---
+++ chap13-data_structure_selection/ex13-1-file_to_words.py
@@ -0,0 +1,28 @@
+# Andrea Cocco 2020
+# Chapter 12 Exercise 1 from the book:
+#     
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpythimport string
+
+"""Write a program that reads a file, breaks each line into words,
+strips whitespace and punctuation from the words, and converts them 
+to lowercase.
+"""
+
+# get the file
+with open('user_manuals.txt') as f:
+    read_data = f.read()
+
+# get rid of punctuation
+for word in read_data:
+    if word in string.punctuation:
+        read_data = read_data.replace(word, '')
+
+# get rid of whitespaces
+for word in read_data:
+    if word in string.whitespace:
+        read_data = read_data.replace(word, '')
+
+print(read_data.lower())on2.com
+#
