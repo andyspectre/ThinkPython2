746917e32d42fee294d5189c50fa4773f09ab682
c0cc0a <coccoa@pm.me>
2021-09-01 18:21:14 +0200

    prepare exercise 13-2

---
+++ chap13-data_structure_selection/ex13-2-the_time_machine.py
@@ -0,0 +1,15 @@
+# Andrea Cocco 2020
+# Chapter 13 Exercise 2 from the book:
+#     
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpythimport string
+
+"""Go to Project Gutenberg and download your favorite out-of-copyright
+book in plain text format. Modify your program from the previous exercise
+to read the book you downloaded, skip over the header information at the
+beginning of the file, and process the rest of the words as before.
+Then modify the program to count the total number of words in the book,
+and the number of times each word is used. Print the number of 
+different words used in the book.
+"""
