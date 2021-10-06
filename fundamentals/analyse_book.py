9202c88eb2e84e5c56342a5407af958342547bb3
Andrea Cocco <coccoa@pm.me>
2021-10-06 18:42:45 +0200

    add list() and most_common()

--- fundamentals/analyse_book.py
+++ fundamentals/analyse_book.py
@@ -1,9 +1,10 @@
 """ Count total number of words and word frequency in a file. """
+
 import string
 from operator import itemgetter
 from collections import Counter
 
-with open('test_book.txt', encoding='UTF-8') as f:
+with open("test.txt", encoding="UTF-8") as f:
     strippables = string.punctuation + string.whitespace
     cnt = Counter()
     for line in f:
@@ -13,8 +14,17 @@ with open('test_book.txt', encoding='UTF-8') as f:
 
     words_total = (sum(cnt.values()))
 
-    print("Total number of words: ", words_total)
-    print("Word frequency:")
 
+    print("Total number of words: ", words_total)
+    print("Total number of different words: ", len(list(cnt)))
+    print()
+    print("Most common 3 words: ")
+
+    for word, freq in(cnt.most_common(3)):
+        print(f"{word:15} ==> {freq:5}")
+    
+    print()
+    print("Word frequency:")
+    
     for word, freq in sorted(cnt.items(), key=itemgetter(1), reverse=True):
-        print(f'{word:15} ==> {freq:5}')
+        print(f"{word:15} ==> {freq:5}")
