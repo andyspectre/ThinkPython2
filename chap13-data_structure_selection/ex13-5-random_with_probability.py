1fbd6a5e2155bdbc586b781bfc3e0dc85cf40c37
Andrea Cocco <coccoa@pm.me>
2021-10-11 23:44:07 +0200

    add exercise 13-5

---
+++ chap13-data_structure_selection/ex13-5-random_with_probability.py
@@ -0,0 +1,17 @@
+import random
+from collections import Counter
+
+def histogram(string):
+
+    cnt = Counter()
+    for letter in string:
+        cnt[letter] += 1
+
+    l = list(cnt)    # unique letters
+    v = list(cnt.values())
+    for i in random.choices(l,v,k=1):
+        return i # random.choices(l, v, k=1)
+
+string = "abracadabra"
+result = histogram(string)
+print(result)
