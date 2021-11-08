8ae70f40db71f916e759e1020e3bbfe4d8ceb4b7
0xc0cc0 <andreacocco@pm.me>
2021-11-08 17:32:14 +0100

    add conter to check outcome against probability

--- chap13-data_structure_selection/ex13-5-random_with_probability.py
+++ chap13-data_structure_selection/ex13-5-random_with_probability.py
@@ -1,3 +1,10 @@
+# Andrea Cocco 2020
+# Chapter 13 Exercise 5 from the book:
+#     
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpython2.com
+
 import random
 from collections import Counter
 
@@ -8,11 +15,15 @@ def histogram(string):
         cnt[letter] += 1
 
     l = list(cnt)    # unique letters
-    v = list(cnt.values())
-    for i in random.choices(l,v,k=1):
-        return i # random.choices(l, v, k=1)
+    v = list(cnt.values())    # letters frequency
+    for i in random.choices(l,weights=v,k=1):
+        return i
 
 string = "abracadabra"
-result = histogram(string)
-print(result)
-# test1
+cnt = Counter()
+
+# try a 100 times and see if the outcome matches more or less the probability
+for i in range(100):
+    cnt[histogram(string)]+=1
+
+print(cnt)
