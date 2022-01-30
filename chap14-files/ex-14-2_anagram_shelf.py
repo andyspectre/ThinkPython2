8d5cecb67c5b8b906d99986cafa0f7d7e4d1e592
0xc0cc0 <andreacocco@pm.me>
2022-01-30 23:02:42 +0100

    add exercise 2

---
+++ chap14-files/ex-14-2_anagram_shelf.py
@@ -0,0 +1,12 @@
+# Andrea Cocco 2022
+# Chapter l4 Exercise 2 from the book:
+#     
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpython2.com
+#
+
+from anagrams import key_getter 
+
+s1 = "post"
+print(key_getter(s1))
