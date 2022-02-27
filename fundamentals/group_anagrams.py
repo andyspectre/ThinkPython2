7e2b22c05b481d59fa5879e28054bc5f65524222
0xc0cc0 <andreacocco@pm.me>
2022-02-27 17:30:14 +0100

    Copied from chapter 12

--- fundamentals/group_anagrams.py
+++ fundamentals/group_anagrams.py
@@ -1,57 +1,89 @@
-""" Group anagrams. Reads a word list from a file and group all the
-    anagrams together.
-    1. Read file and initialize a list of strings with it
-    2. key_getter(word)
-        for each letter of the word:
-            get its equivalent unicode number 
-            put it in a tuple called key
-    3. more_anagrams(tuple, word):
-           initialize dictionary
-           if tuple is not in dictionary:
-               key is the tuple and value is word
-           else:
-               add word to that key
-        key = tuple with numeric representation of the letters of the strings
-        value = list of words that match that key
-"""
+# Andrea Cocco 2020
+# Chapter 12 Exercise 2 from the book:
+#     
+# Think Python, 2nd Edition
+# by Allen Downey
+# http://thinkpython2.com
+#
+# *******************************************
+# Get a list of anagrams from a list of words 
+# *******************************************
+
+import string
+
 def result(d):
+    # Return a list of words that are anagrams
     r = []
-    for key in d:
-        if len(d[key]) > 1:
-            r.append(d[key])
-    return r
+    for v in d.values():
+        # we want only the elements in the dictionary that have at least
+        # two words, that is they are anagrams
+        if len(v) > 1:
+            r.append((len(v),v))
+    r.sort()
 
-def key_getter(strs):
-    key = [0] * 26
+    for i in r:
+        print(i)
 
-    for letter in strs:
-        if letter.islower():
-            start = ord('a')
-        elif letter.isupper():
-            start = ord('A')
-        else:
-            return letter
-        position = ord(letter) - start
-        key[position] += 1
-    
-    key = tuple(key)
-    return key
+  # -----------------------------------------
+  # Alternative method instead of signature()
+  # -----------------------------------------
+  #  def key_getter(strs):
+  #  # Return a tuple of "markers" that mark which letters
+  #  # appear in a string. These markers will be the keyes of a dictionary
+  #  key = [0] * 26    # Todo: fix magic constant: 26 
+  #  for letter in strs:
+  #      # get the unicode representation of the letters of the words
+  #      if letter.islower():
+  #          start = ord('a')
+  #      elif letter.isupper():
+  #          start = ord('A')
+  #      position = ord(letter) - start
+  #      key[position] += 1    # mark the letters (just add '1' in the list)
+  #  # convert list into tuple so that can be used as key in a dictionary
+  #  key = tuple(key)    
+  #  return key
+
+def signature(w):
+    t = sorted(w)
+    t = ''.join(t)
+    return t
 
 def anagram(li):
-    d = dict()
-    
-    for word in li:
-        key = key_getter(word)
-        d.setdefault(key,[]).append(word)
-    return(result(d))
+    # Accept a list of strings as argument and return a dictionary 
+    # in which the key is a tuple of "markers" representing the letters 
+    # of the words and the value is the actual word.
 
-def create_list(strs):
+    d = dict()
+
+    for word in li:
+        # key = key_getter(word)
+        w = signature(word) 
+
+        if w not in d:
+            d[w] = [word]
+        else:
+            d[w].append(word)
+
+    return result(d)
+
+def create_list(f):
+    # Accept a file with one string per line and return a list
+    # of strings.
+
+    # dict to get rid of punctuation with translate
+    d = dict()
+    for i in string.punctuation:
+        d[i] = ''
+
+    table = str.maketrans(d)
     li = []
-    for line in strs:
-        word = line.strip()
-        li.append(word)
+    for line in f:
+        for word in line.split():
+            word = word.translate(table)
+            li.append(word)
     return anagram(li)
 
-fin = open("test_anagrams.txt")
-print(create_list(fin))
+if __name__ == '__main__':
 
+    with open("names.txt", encoding="UTF-8") as f:
+        create_list(f)
