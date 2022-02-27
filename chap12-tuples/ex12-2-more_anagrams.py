8753c1f1cbc6a6610366b474e4afba760e19e784
0xc0cc0 <andreacocco@pm.me>
2022-02-27 17:23:27 +0100

    add alternative method: signature() and modify result()

--- chap12-tuples/ex12-2-more_anagrams.py
+++ chap12-tuples/ex12-2-more_anagrams.py
@@ -5,7 +5,8 @@
 # by Allen Downey
 # http://thinkpython2.com
 #
-""" 1. Read file and initialize a list of strings with it
+"""
+    1. Read file and initialize a list of strings with it
     2. key_getter(word)
         for each letter of the word:
             get its equivalent unicode number 
@@ -22,59 +23,78 @@
 import string
 
 def result(d):
-    """ Return a list of words that are anagrams """
+    # Return a list of words that are anagrams
     r = []
-    for key in d:
+    for v in d.values():
         # we want only the elements in the dictionary that have at least
         # two words, that is they are anagrams
-        if len(d[key]) > 1:
-            r.append(d[key])
-    return r
+        if len(v) > 1:
+            r.append((len(v),v))
+    r.sort()
 
-def key_getter(strs):
-    """ Return a tuple of "markers" that mark which letters
-    appear in a string. These markers will be the keyes of a dictionary
-    """
-    key = [0] * 26    # initialize a list of 26 "0" (our markers)
-    for letter in strs:
-        # get the unicode representation of the letters of the words
-        if letter.islower():
-            start = ord('a')
-        elif letter.isupper():
-            start = ord('A')
-        position = ord(letter) - start
+    for i in r:
+        print(i)
 
-        key[position] += 1    # mark the letters (just add '1' in the list)
+  # ----------------------------
+  # Alternative method instead of signature()
+  # ----------------------------
+  #  def key_getter(strs):
+  #  # Return a tuple of "markers" that mark which letters
+  #  # appear in a string. These markers will be the keyes of a dictionary
+  #  key = [0] * 26    # Todo: fix magic constant: 26 
+  #  for letter in strs:
+  #      # get the unicode representation of the letters of the words
+  #      if letter.islower():
+  #          start = ord('a')
+  #      elif letter.isupper():
+  #          start = ord('A')
+  #      position = ord(letter) - start
+  #      key[position] += 1    # mark the letters (just add '1' in the list)
+  #  # convert list into tuple so that can be used as key in a dictionary
+  #  key = tuple(key)    
+  #  return key
 
-    # convert list into tuple so that can be used as key in a dictionary
-    key = tuple(key)    
-    return key
+def signature(w):
+    t = sorted(w)
+    t = ''.join(t)
+    return t
 
 def anagram(li):
-    """ Accept a list of strings as argument and return a dictionary 
-    in which the key is a tuple of "markers" representing the letters 
-    of the words and the value is the actual word.
-    """
+    # Accept a list of strings as argument and return a dictionary 
+    # in which the key is a tuple of "markers" representing the letters 
+    # of the words and the value is the actual word.
+
     d = dict()
 
     for word in li:
-        key = key_getter(word)
-        if key not in d:
-            d[key] = [word]
+        # key = key_getter(word)
+        w = signature(word) 
+
+        if w not in d:
+            d[w] = [word]
         else:
-            d[key].append(word)
+            d[w].append(word)
+
     return result(d)
 
-def create_list(strs):
-    """ Accept a file with one string per line and return a list
-    of strings.
-    """
+def create_list(f):
+    # Accept a file with one string per line and return a list
+    # of strings.
+
+    # dict to get rid of punctuation with translate
+    d = dict()
+    for i in string.punctuation:
+        d[i] = ''
+
+    table = str.maketrans(d)
     li = []
-    for line in strs:
-        word = line.strip()
-        li.append(word)
+    for line in f:
+        for word in line.split():
+            word = word.translate(table)
+            li.append(word)
     return anagram(li)
 
+if __name__ == '__main__':
 
-fin = open("pass_list.txt")
-print(create_list(fin))
+    with open("names.txt", encoding="UTF-8") as f:
+        create_list(f)
